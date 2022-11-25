17aa4a6670ef7dbdb050e75a0eb74dc59ee52621
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-11-25 08:37:51 +0100

    implemented BookController

---
+++ src/main/java/com/chixsaw/spring5webapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.chixsaw.spring5webapp.controllers;
+
+import com.chixsaw.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Raphael Frey on 25.11.2022
+ */
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
